437521b0fb8b5134497b170ed79a30679db8fbea
fukunaga-kaede <fkngmomijik@yahoo.co.jp>
2021-11-09 12:30:51 +0900

    11/9　2限

--- app/src/main/java/com/example/kaede1/Input.java
+++ app/src/main/java/com/example/kaede1/Input.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class Input extends AppCompatActivity {
 
@@ -33,11 +36,43 @@ public class Input extends AppCompatActivity {
         public void onClick (View view) {
 
             // ラジオボタンの内容で金額場合分け
+            RadioGroup radioGroup = (RadioGroup) findViewById(R.id.flgIncomeExpenditure);
+
+            int checkedId = radioGroup.getCheckedRadioButtonId();
+
+            if (checkedId != -1) {
+                // 選択されているラジオボタンの取得
+                RadioButton radioButton = (RadioButton) findViewById(checkedId);
+
+                // ラジオボタンのテキストを取得
+                String text = radioButton.getText().toString();
+
+                // 入力内容を取得
+                TextView inputDateText = findViewById(R.id.inputDate);
+                TextView inputItemText = findViewById(R.id.inputItem);
+                TextView inputItemAmount = findViewById(R.id.inputAmount);
+                TextView inputItemMemo = findViewById(R.id.inputMemo);
+
+                String inputDate = (String)inputDateText.getText();
+                String inputItem = (String)inputItemText.getText();
+                String inputAmount = (String)inputItemAmount.getText();
+                String inputMemo = (String)inputItemMemo.getText();
+
+                // 金額の符号を設定
+                if()
+
+                // SQL
+
+
+            } else {
+                // 何も選択されていない場合の処理
+                // トーストを作る
+            }
 
 
             // DBの更新処理
 
-            finish();
+            // finish();
         }
     }
 }
